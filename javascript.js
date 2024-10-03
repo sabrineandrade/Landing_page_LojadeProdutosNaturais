469fba7c3c97d1cfaa0278c921e8565d8f104982
sabrineandrade <138935834+sabrineandrade@users.noreply.github.com>
2024-10-03 15:29:47 -0300

    adicionando conteudo

---
+++ javascript.js
@@ -0,0 +1,26 @@
+var setadireita = window.document.getElementById("setadireita");
+var feedback1 = window.document.getElementById("feedback1");
+var feedback2 = window.document.getElementById("feedback2");
+var feedback3 = window.document.getElementById("feedback3");
+var setaesquerda = window.document.getElementById("setaesquerda");
+
+var botao = window.document.getElementsByClassName("butao");
+
+function rolarparadireita() {
+  feedback1.style = "display:none";
+  feedback3.style = "display:flex";
+  setadireita.style = "display:none";
+  setaesquerda.style = "display: flex";
+}
+
+function rolarparaesquerda() {
+  feedback1.style = "display:flex";
+  feedback3.style = "display:none";
+  setaesquerda.style = "display:flex";
+  setadireita.style = "display: flex";
+}
+
+ScrollReveal().reveal("#formulario");
+ScrollReveal().reveal(".feedback");
+ScrollReveal().reveal("#segunda-parte");
+ScrollReveal().reveal("#fundo");
